a99792b6cb2bcc56a46669b4b7442ebd30f16524
Peter Cardenas <petermartincardenas@gmail.com>
2026-09-21 20:23:21 +0800

    Round shift pay instead of truncating - cardenas.peter

--- shifts.js
+++ shifts.js
@@ -4,11 +4,11 @@ function isValidShift(hours) {
 
 function calculatePay(hours, rate) {
   if (hours > 8) {
-    const regularpay = 8 * rate;
+    const regularPay = Math.round(8 * rate);
     const overtimePay = (hours - 8) * (rate * 1.5);
-    return Math.floor(regularPay + overtimePay);
+    return Math.round(regularPay + overtimePay);
   }
-  return Math.floor(hours * rate);
+  return Math.round(hours * rate);
 }
 
 module.exports = { isValidShift, calculatePay };
