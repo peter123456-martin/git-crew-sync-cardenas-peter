3e0932ec6ce9beb91bd451fca4d61a9ca920656f
Peter Cardenas <petermartincardenas@gmail.com>
2026-09-21 20:38:43 +0800

    Tweak regular pay calculation - cardenas.peter

--- shifts.js
+++ shifts.js
@@ -5,9 +5,8 @@ function isValidShift(hours) {
 function calculatePay(hours, rate) {
   if (hours > 8) {
 
-    const regularPay = Math.round(8 * rate);
 
-    
+    const regularPay = Math.round(8 * rate);
 
     const overtimePay = (hours - 8) * (rate * 1.5);
     return Math.round(regularPay + overtimePay);
